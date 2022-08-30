73c9d9725af4814f92b5e778d2e781cc3a9e3f08
Shreesaran Satapathy <shrees@ad.unc.edu>
2022-08-29 20:38:58 -0400

    Chardle changes

--- exercises 2/ex01_chardle.py
+++ exercises 2/ex01_chardle.py
@@ -4,30 +4,32 @@ __author__ = "730616899"
 
 word: str = input("Enter a 5-character word. ")
 if len(word) != 5: 
-    exit("Error: Word must contain 5 characters")
+    exit("Error Word must contain 5 characters")
 
 character: str = input("Enter a single character. ")
 if len(character) != 1:
-    exit("Error: Character must be a single character.")
+    exit("Error: Character must be a single character")
 
 count: int = 0
 
+print("Enter a 5-character word: " + word)
+print("Enter a single character: " + character)
 print("Searching for " + character + " in " + word)
 
 if word[0] == character:
-    print(character + " found at index 0.")
+    print (character + " found at index 0.")
     count = count + 1
 if word[1] == character:
-    print(character + " found at index 1.")
+    print (character + " found at index 1.")
     count = count + 1
 if word[2] == character:
-    print(character + " found at index 2.")
+    print (character + " found at index 2.")
     count = count + 1
 if word[3] == character:
-    print(character + " found at index 3.")
+    print (character + " found at index 3.")
     count = count + 1
 if word[4] == character:
-    print(character + " found at index 4.")
+    print (character + " found at index 4.")
     count = count + 1
 
 if count == 0: 
